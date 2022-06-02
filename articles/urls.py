75a4bedfd3f01071eac1779c298aefe8a5211f36
juhyun1011 <juhyun4567@gmail.com>
2022-06-03 01:17:45 +0900

    Update

---
+++ articles/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from articles import views
+
+
+app_name = 'articles'
+urlpatterns = [    
+    path('index/', views.index),
+    path('dinner/<str:name>', views.dinner),
+    path('review/', views.review, name='review'),
+    path('create_review/', views.create_review, name='create_review'),
+
+]
