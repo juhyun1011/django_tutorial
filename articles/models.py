62c48e0d149cceecd61834a7d9ceb762c666857f
juhyun1011 <juhyun4567@gmail.com>
2022-06-03 21:26:31 +0900

    admin으로 CRUD

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,10 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+
+class Article(models.Model):
+    # id는 자동으로 만들어진다.
+    title = models.CharField(max_length=10)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
