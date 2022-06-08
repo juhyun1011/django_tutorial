d4624988cf2beba5a409b81c82b12cb9fa4f9363
juhyun1011 <juhyun4567@gmail.com>
2022-06-09 00:38:12 +0900

    게시글 update

--- articles/views.py
+++ articles/views.py
@@ -48,5 +48,19 @@ def delete(request, pk):
 
     if request.method == 'POST':
         article.delete()
-        
+
     return redirect('articles:dinner')
+
+def edit(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article' : article,
+    }
+    return render(request, 'edit.html', context)
+
+def update(request, pk):
+    article = Article.objects.get(pk=pk)
+    article.title = request.POST.get('title')
+    article.content = request.POST.get('content')
+    article.save()
+    return redirect('articles:detail', article.pk)
